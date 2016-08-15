aa1739a7d4660c2663609469c903212ad0f52b54
nimstill <dandelionstill@gmail.com>
2016-08-15 09:21:39 +0800

    learn

---
+++ some lea.py
@@ -0,0 +1,14 @@
+import re 
+import urllib2
+
+import time 
+from Queue import Queueimport threading, errno, datetime
+
+import json import requests # requests is an Apache2 Licensed HTTP library
+import MySQLdb as mdb
+
+DB_HOST = '129.0.0.1'
+DB_USER = 'root'
+DB_PASS = ''
+
+
