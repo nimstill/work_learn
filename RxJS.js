e3f625374c88ad2d5dce7bd8714a1f0c6dc1c7a6
nimstill <dandelionstill@gmail.com>
2017-03-16 17:54:46 +0800

    Rx

--- RxJS.js
+++ RxJS.js
@@ -46,3 +46,22 @@ var inputStream = Rx.Observable.fromEvent(text, 'keyup')
 					.subscribe(date => render(date));
 
 
+订阅：Observer 通过 Observable 提供的 subscribe() 方法订阅 Observable。
+发布：Observable 通过回调 next 方法向 Observer 发布事件。
+
+
+Rx.Observable.of(2)
+			 .map(v => v*2)
+			 .subscribe(v => console.log(v));
+
+
+var text = document.querySelector('#text');
+Rx.Observable.fromEvent(text, 'keyup')
+			 .debounceTime(250)
+			 .pluck('target', 'value')
+			 .switchMap(url => Http.get(url))
+			 .subscribe(data => render(data))
+
+
+http://www.alloyteam.com/2016/12/learn-rxjs/
+
