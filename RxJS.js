7a4cbf5842e3182fed6c14787e7bdb0483c2624b
nimstill <dandelionstill@gmail.com>
2017-03-16 16:06:39 +0800

    Rx

--- RxJS.js
+++ RxJS.js
@@ -10,5 +10,39 @@
     })
 </script>
 
+<input id="text"></input>
+<script >
+	
+	var text = document.querySelector('#text'),
+		timer = null,
+		currentSearch = '';
+
+	text.addEventListener('keyup', (e) => {
+		clearTimeout(timer)
+		timer = setTimeout(() => {
+			currentSearch = 'book';
+			var searchText = e.target.value;
+			$.ajax({
+				url: ``,
+				success: date => {
+					if (date.search === currentSearch) {
+						render(date);
+					} else {
+						//
+					}
+				}
+			})
+		}, 250)
+	})
+
+
+</script>
+
+var text = document.querySelector('#text');
+var inputStream = Rx.Observable.fromEvent(text, 'keyup')
+					.debounceTime(250)
+					.pluck('target', 'value')
+					.switchMap(url => Http.get(url))
+					.subscribe(date => render(date));
 
 
