89137f6b4a078b3ee5357acfbec7c34d8a961de9
nimstill <dandelionstill@gmail.com>
2017-05-25 14:11:12 +0800

    ES6 Proxy

--- ES6.js
+++ ES6.js
@@ -19,3 +19,30 @@ for (let i of text) {
 .at()
 
 
+/**
+ * ES6 Proxy
+ */
+
+let obj = new Proxy({}, {
+	ger (target, key, receiver) {
+		console.log(`getting ${key}!`)
+		return Reflect.get(target, key, receiver)
+	},
+	set (target, key, value, receiver) {
+		console.log(`setting ${key}!`)
+		return Reflect.set(targer, key, value, receiver)
+	}
+})
+
+var proxy = new Proxy({}, {
+  get: function(target, property) {
+    return 35;
+  }
+});
+
+proxy.time // 35
+proxy.name // 35
+proxy.title // 35
+
+要使得Proxy起作用，必须针对Proxy实例（上例是proxy对象）进行操作，而不是针对目标对象（上例是空对象）进行操作。
+
